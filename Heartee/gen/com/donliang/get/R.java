50082cae4a3fa0a873d94ba1d606fc72e94a2acc
Don Liang <duolingl@gmail.com>
2014-07-22 17:26:29 -0700

    adding listview together with addHeaderView() in SlidingMenu

--- Heartee/gen/com/donliang/get/R.java
+++ Heartee/gen/com/donliang/get/R.java
@@ -8,6 +8,9 @@
 package com.donliang.get;
 
 public final class R {
+    public static final class array {
+        public static final int menu_item=0x7f080000;
+    }
     public static final class attr {
         /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
 Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
@@ -123,6 +126,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          */
         public static final int viewBehind=0x7f010002;
     }
+    public static final class color {
+        public static final int Drawer_background=0x7f050001;
+        public static final int Item_textcolor=0x7f050002;
+        public static final int NavDrawer_MainContent_BackGround=0x7f050000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -131,35 +139,40 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f040007;
+        public static final int action_settings=0x7f04000a;
         public static final int fullscreen=0x7f040003;
         public static final int left=0x7f040000;
+        public static final int left_drawer=0x7f040006;
         public static final int margin=0x7f040002;
         public static final int none=0x7f040004;
         public static final int right=0x7f040001;
         public static final int selected_view=0x7f040005;
-        public static final int slidingmenumain=0x7f040006;
+        public static final int slidingmenumain=0x7f040009;
+        public static final int textView1=0x7f040008;
+        public static final int txtItem=0x7f040007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_menu=0x7f030001;
-        public static final int fragment_main=0x7f030002;
-        public static final int slidingmenumain=0x7f030003;
+        public static final int drawer_list_item=0x7f030002;
+        public static final int fragment_main=0x7f030003;
+        public static final int headerview=0x7f030004;
+        public static final int slidingmenumain=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f0a0000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
     }
     public static final class style {
         /** 
@@ -183,11 +196,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f090001;
     }
     public static final class styleable {
         /** Attributes that can be used with a SlidingMenu.
--- Heartee/src/com/donliang/get/MainActivity.java
+++ Heartee/src/com/donliang/get/MainActivity.java
@@ -5,28 +5,46 @@ import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.os.Build;
 
 public class MainActivity extends SlidingActivity {
 
+	private ListView SlidingMenu_List;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		setBehindContentView(R.layout.activity_menu);
-
-		getSlidingMenu().setBehindOffset(100);
+		initSlidngMenuLV();
+		
 //		if (savedInstanceState == null) {
 //			getFragmentManager().beginTransaction()
 //					.add(R.id.container, new PlaceholderFragment()).commit();
 //		}
+	}
+
+	private void initSlidngMenuLV() {
+		// TODO Auto-generated method stub
+		setBehindContentView(R.layout.activity_menu);
+		getSlidingMenu().setBehindOffset(100);
+		
+		SlidingMenu_List = (ListView) findViewById(R.id.left_drawer);
+		LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(R.layout.headerview, null);
+		SlidingMenu_List.addHeaderView(view);
+		
+        String[] drawer_menu = this.getResources().getStringArray(R.array.menu_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawer_menu);
+        SlidingMenu_List.setAdapter(adapter);
 	}
 
 	@Override
