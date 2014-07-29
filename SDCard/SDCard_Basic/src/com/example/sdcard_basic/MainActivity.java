21a20de30b8f4f9c84640405ce490f6fd1384644
Don Liang <duolingl@gmail.com>
2014-07-28 18:14:29 -0700

    Check SD Card first

--- SDCard/SDCard_Basic/src/com/example/sdcard_basic/MainActivity.java
+++ SDCard/SDCard_Basic/src/com/example/sdcard_basic/MainActivity.java
@@ -1,70 +1,30 @@
 package com.example.sdcard_basic;
 
+import java.util.List;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Environment;
 import android.widget.TextView;
-import android.os.Build;
+
 
 public class MainActivity extends Activity {
+    
+	TextView textview;
 	
-	private TextView tv;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        this.tv = (TextView)findViewById(R.id.tv);
-
-//        if (savedInstanceState == null) {
-//            getFragmentManager().beginTransaction()
-//                    .add(R.id.container, new PlaceholderFragment())
-//                    .commit();
-//        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+        textview = (TextView)findViewById(R.id.textView1);
         
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_REMOVED)) {
+        	return;
+        } else { 
+        	textview.setText("SD Card is here.");
         }
     }
-
+   
 }
